8bcf20f9d848e1d0f57f694ea5141cb435934070
saiki <otoiawasehakotiradayo@gmail.com>
2018-06-10 00:31:47 +0900

    一通り実装

--- app/src/main/java/app/saiki/navigationsample/MainActivityFragment.kt
+++ app/src/main/java/app/saiki/navigationsample/MainActivityFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,5 +16,13 @@ class MainActivityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_mainActivityFragment_to_subFragment)
+        }
     }
 }
